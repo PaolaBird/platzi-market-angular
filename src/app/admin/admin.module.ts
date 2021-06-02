6fbde17e595030a7faf174c6a86f02c57d2ffc53
Paola Pajaro <yindypaolapu@ufps.edu.co>
2021-06-02 09:47:00 -0500

    Se hac implementación primaria de pruebas unitarias con jasmine y karma

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,22 +6,12 @@ import {ProductFormComponent} from './components/product-form/product-form.compo
 import {MatCardModule} from '@angular/material/card';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from '../material/material.module';
-import { NavComponent } from './components/nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { TableComponent } from './components/table/table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import {NavComponent} from './components/nav/nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {TableComponent} from './components/table/table.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {ProductEditComponent} from './components/product-edit/product-edit.component';
 
 
 @NgModule({
--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,16 +1,68 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ProductsService } from './products.service';
+
+import {ProductsService} from './products.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../../environments/environment';
 
 describe('ProductService', () => {
   let service: ProductsService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule]});
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('test for getAllProducts', () => {
+
+    it('should return products list', () => {
+      // Arrange
+      const expectData = [
+        {
+          id: '1',
+          image: 'assets/images/camiseta.png',
+          title: 'Camiseta',
+          price: 80000,
+          description: 'bla bla bla bla bla'
+        },
+        {
+          id: '2',
+          image: 'assets/images/camiseta.png',
+          title: 'Pantalon',
+          price: 80000,
+          description: 'bla bla bla bla bla'
+        }
+      ];
+
+      let dataError;
+      let dataResponse;
+
+      // Act
+      service.getAllProducts().subscribe(response => {
+        dataResponse = response;
+      }, error => {
+        dataError = error;
+      });
+
+      const req = httpTestingController.expectOne(`${environment.url_api}/products`);
+      req.flush(expectData);
+
+      // Assert
+
+      expect(dataResponse.length).toEqual(2);
+      expect(req.request.method).toEqual('GET');
+      expect(dataError).toBeUndefined();
+
+    });
+  });
+
 });
